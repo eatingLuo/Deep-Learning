a6057e42a949b784be91af8951d2e78174c43245
Amir Jafari <amir.h.jafari@okstate.edu>
2017-11-15 12:37:33 -0500

    Updated

--- Tenflow_/Mini_Project/ptb_word_lm.py
+++ Tenflow_/Mini_Project/ptb_word_lm.py
@@ -259,22 +259,22 @@ class LargeConfig(object):
   batch_size = 20
   vocab_size = 10000
 
-
-class TestConfig(object):
-  """Tiny config, for testing."""
-  init_scale = 0.1
-  learning_rate = 1.0
-  max_grad_norm = 1
-  num_layers = 1
-  num_steps = 2
-  hidden_size = 2
-  max_epoch = 1
-  max_max_epoch = 1
-  keep_prob = 1.0
-  lr_decay = 0.5
-  batch_size = 20
-  vocab_size = 10000
-
+#
+#class TestConfig(object):
+#  """Tiny config, for testing."""
+#  init_scale = 0.1
+#  learning_rate = 1.0
+#  max_grad_norm = 1
+#  num_layers = 1
+#  num_steps = 2
+#  hidden_size = 2
+#  max_epoch = 1
+#  max_max_epoch = 1
+#  keep_prob = 1.0
+#  lr_decay = 0.5
+#  batch_size = 20
+#  vocab_size = 10000
+#
 
 def run_epoch(session, model, eval_op=None, verbose=False):
   """Runs the model on the given data."""
--- Tenflow_/Mini_Project/seq2seq.py
+++ Tenflow_/Mini_Project/seq2seq.py
@@ -19,4 +19,4 @@ from __future__ import division
 from __future__ import print_function
 
 # pylint: disable=wildcard-import
-from tensorflow.python.ops.seq2seq import *
+# from tensorflow.python.ops.seq2seq import *
