d33269e83debfaa5939db3a4ec73640d8b1fc0bc
amir-jafari <amir.h.jafari@okstate.edu>
2018-02-17 18:05:20 -0500

    Updated

--- Pytorch_/pytoch_basics/Exercise_1.py
+++ Pytorch_/pytoch_basics/Exercise_1.py
@@ -1,14 +1,17 @@
-#----------------------------------------------------------------------------'
+# ----------------------------------------------------------------------------'
+# Q1:  Rewrite the 2_tensor_pytorch file in our notation and find out what are the
+# differences. Find out which was easier to follow?.
 
-# Q1:  Use the time package python library and on 1_Numpy and 2_Tensor_Pytorch files. To find out
+
+# ----------------------------------------------------------------------------'
+# Q2:  Use the time package python library on 1_Numpy and 2_Tensor_Pytorch files. To find out
 # numpy is faster or torch tensor. Keep the same data size and change the number of epochs.
 #  Check your results ans see does it make sense.
 
 
 
 
-#----------------------------------------------------------------------------'
-
-# Q2:  Use the time package python library and on 1_Numpy and 2_Tensor_Pytorch files. To find out
+# ----------------------------------------------------------------------------'
+# Q3:  Use the time package python library in 1_Numpy and 2_Tensor_Pytorch files. To find out
 # numpy is faster or torch tensor GPU. Keep the same data size and change the number of epochs.
 #  Check your results ans see does it make sense.
