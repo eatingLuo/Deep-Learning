6a87a56605d5cb388ed05558174ac7a8f1c07caf
amir-jafari <amir.h.jafari@okstate.edu>
2018-02-19 16:15:29 -0500

    Updated

--- Pytorch_/2_nn_module/Exercise_3.py
+++ Pytorch_/2_nn_module/Exercise_3.py
@@ -1,3 +1,17 @@
 #----------------------------------------------------------------------------'
 
-# Q1: check if th
+# Q1: Check if any of pytorch code model and Varibles are using cuda capabilities.
+
+
+
+
+#----------------------------------------------------------------------------'
+
+# Q2: Train a 2 layer mlp network for input -3<p<3 and underlying function t= sin(p)
+
+
+
+
+#----------------------------------------------------------------------------'
+
+# Q3: Train a 4 layer mlp network for input -3<p<3 and underlying function t= sin(p)
